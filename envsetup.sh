352f10760d3dc2d393acea83dcdeefd5157b0b3a
Matt Mower <mowerm@gmail.com>
2017-01-02 02:31:27 -0600

    envsetup: Tweak nested executed commands
    $(( causes some text editors to expect arithmetic is incoming and then
    syntax highlighting is thrown off for the rest of the script due to
    missing )). Include a space before the subshell command to help
    distinguish the nested command.
    
    Change-Id: I5bae8f32b3c70ac460681fcfde1df0febbe554fd

--- envsetup.sh
+++ envsetup.sh
@@ -134,7 +134,7 @@ function check_product()
 
     if (echo -n $1 | grep -q -e "^xenonhd_") ; then
        XENONHD_BUILD=$(echo -n $1 | sed -e 's/^xenonhd_//g')
-       export BUILD_NUMBER=$((date +%s%N ; echo $XENONHD_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
+       export BUILD_NUMBER=$( (date +%s%N ; echo $XENONHD_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
        XENONHD_BUILD=
     fi
